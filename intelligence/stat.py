f13f131f06277e79b7445480caccacbcf95747d8
astex <0astex@gmail.com>
2017-04-29 11:51:07 -0400

    Add a normal distribution.

---
+++ intelligence/stat.py
@@ -0,0 +1,24 @@
+"""Statistics utilities."""
+
+
+import numpy as np
+
+
+class NormalDistribution(object):
+    """A normal distribution."""
+
+    def __init__(self, mean, standard_deviation):
+        self.mean = mean
+        self.standard_deviation = standard_deviation
+
+    @property
+    def variance(self):
+        return self.standard_deviation ** 2
+
+    def __call__(self, x):
+        return (
+            1 / np.sqrt(2 * np.pi * self.variance) *
+            np.exp(- (x - self.mean) ** 2 / 2 * self.variance))
+
+
+GaussianDistribution = NormalDistribution
